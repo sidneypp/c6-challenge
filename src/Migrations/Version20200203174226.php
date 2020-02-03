63e7c846705fc39cd2417eef7399c583bd0a1d29
Sidney Oliveira <oliveira.sidney96@gmail.com>
2020-02-03 19:30:21 -0300

    Create new migration

---
+++ src/Migrations/Version20200203174226.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200203174226 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(500) NOT NULL, created_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_1483A5E9F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE persons (id INT AUTO_INCREMENT NOT NULL, personid INT NOT NULL, personname VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE phones (id INT AUTO_INCREMENT NOT NULL, person_id INT DEFAULT NULL, phone VARCHAR(15) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_E3282EF5217BBB47 (person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE phones ADD CONSTRAINT FK_E3282EF5217BBB47 FOREIGN KEY (person_id) REFERENCES persons (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE phones DROP FOREIGN KEY FK_E3282EF5217BBB47');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE persons');
+        $this->addSql('DROP TABLE phones');
+    }
+}
